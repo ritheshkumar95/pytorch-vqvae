76c7ed3981ef715c2c5e88533345adba1592ccd3
eracah <ejracah@gmail.com>
2018-05-04 00:41:58 -0400

    changed dataset to be ImageFolder and save out to jpgs

--- video_vq_vae.py
+++ video_vq_vae.py
@@ -16,32 +16,82 @@ import gym
 from PIL import Image
 from torchvision.transforms import Compose,Normalize,Resize,ToTensor
 from matplotlib import pyplot as plt
+#%matplotlib inline
 from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import ImageFolder
+import argparse
+from tensorboardX import SummaryWriter
+
+
+# In[2]:
+
+
+import sys
+if __name__ == "__main__":
+    test_notebook = False
+    if "ipykernel_launcher" in sys.argv[0]:
+        sys.argv = [""]
+        test_notebook= True
+
+
+# In[17]:
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size",type=int,default=4)
+    parser.add_argument("--epochs",type=int,default=100)
+    parser.add_argument("--num_workers",type=int,default=4)
+    parser.add_argument("--num_frames",type=int,default=60000)
+    parser.add_argument("--resize_to",type=int,default=128) # paper is 100
+    parser.add_argument("--data_dir",type=str,default="../data")
+    parser.add_argument("--savedir",type=str,default=".")
+    parser.add_argument("--dataset",type=str,default="Bowling-v0")
+    parser.add_argument("--lr",type=float,default=3e-4)
+    parser.add_argument("--K",type=int,default=512)
+    parser.add_argument("--dim",type=int,default=256)
+    parser.add_argument("--lambda_",type=int,default=1)
+    args = parser.parse_args()
+
+
+# In[18]:
+
+
+basename="vqvae"
+def mkstr(key):
+    d = args.__dict__
+    return "=".join([key,str(d[key])])
+
+output_dirname = "_".join([basename,*[mkstr(k) for k in args.__dict__ if "dir" not in k]])
+
+
+# In[19]:
+
+
+if test_notebook:
+    output_dirname = "notebook_" + output_dirname
+    
+saved_model_dir = os.path.join(args.savedir,("models/%s" % output_dirname))
+
+log_dir = os.path.join(args.savedir,'.%s_logs/%s'%(basename,output_dirname))
+
+writer = SummaryWriter(log_dir=log_dir)
+
+for dir_ in [saved_model_dir, args.data_dir, log_dir]:
+    if not os.path.exists(dir_):
+        os.makedirs(dir_)
+
+
+# In[20]:
+
 
 env_list = ["Skiing-v0","Tennis-v0","Pong-v0","Kangaroo-v0","CrazyClimber-v0","Breakout-v0","Bowling-v0"]
 
-env_name = "Pong-v0"
-
-BATCH_SIZE = 32
-N_EPOCHS = 100
 PRINT_INTERVAL = 100
-DATASET =  "Pong-v0"#'CIFAR10'  # CIFAR10 | MNIST | FashionMNIST
-NUM_WORKERS = 4
-num_frames=60000
-resize_to=(128,128)
 INPUT_DIM = 3  # 3 (RGB) | 1 (Grayscale)
-DIM = 256
-K = 512
-LAMDA = 1
-LR = 3e-4
-
 DEVICE = torch.device('cuda') # torch.device('cpu')
 
-data_dir = "../data"
 
-for dir_ in ["models", "samples", data_dir]:
-    if not os.path.exists(dir_):
-        os.mkdir(dir_)
 
 def convert_frame(state, new_shape=(64,64), cuda=True):
     state = Image.fromarray(state, 'RGB')
@@ -50,67 +100,82 @@ def convert_frame(state, new_shape=(64,64), cuda=True):
         transforms = Compose([Resize(new_shape)])
         state = transforms(state)
 
-    return np.asarray(state)
+    return state #Image
 
-def save_frames(env_name,num_frames=60000, resize_to=(128,128)):
+
+# In[21]:
+
+
+def save_frames(env_name,imdir, num_frames=60000, resize_to=128):
     env = gym.make(env_name)
     state = env.reset()
-
-    frames = np.zeros((num_frames,*resize_to,3),dtype=np.uint8)
-
-    actions = np.zeros((num_frames,),dtype=np.uint8)
-
+    done = False
     t0 = time.time()
     for i in range(num_frames):
         action = env.action_space.sample()
-        state,_,_,_ = env.step(action)
-        framen = convert_frame(state,new_shape=resize_to)
-        frames[i] = framen
-        actions[i] = action
+        if done:
+            state = env.reset()
+            done = False
+        else:
+            state,r,done,_ = env.step(action)
+        frame = convert_frame(state,new_shape=(resize_to, resize_to))
+        frame.save(os.path.join(imdir,str(i) + ".jpg"))
+    
     print(time.time() - t0)
 
-    fn = os.path.join(data_dir,env_name + ".npz")
-    fna = os.path.join(data_dir,env_name + "actions_" + ".npz")
-    np.savez(fn,frames=frames)
-    np.savez(fna,actions=actions)
 
-fn = os.path.join(data_dir,DATASET + ".npz")
-fna = os.path.join(data_dir,env_name + "actions_" + ".npz")
+# In[22]:
 
-if not os.path.exists(fn):
-    save_frames(DATASET,num_frames=num_frames,resize_to=resize_to)
 
-class FrameDataset(Dataset):
-    def __init__(self, fn,fna, transform=None, just_frames=False):
-        self.just_frames = just_frames
-        frame_shape = (num_frames,*resize_to,3)
-        self.frame_data = np.memmap(fn, dtype='uint8', mode='r', shape=frame_shape)
-        if not self.just_frames:
-            action_shape = (num_frames,)
-            self.action_data = np.memmap(fna, dtype='uint8', mode='r', shape=action_shape)
+imdir = os.path.join(args.data_dir,args.dataset,str(0))
+if not os.path.exists(imdir):
+    os.makedirs(imdir)
+    save_frames(args.dataset,imdir=imdir, num_frames=args.num_frames,resize_to=args.resize_to)
+
+
+# In[23]:
+
+
+# class FrameDataset(Dataset):
+#     def __init__(self, fn,transform=None, just_frames=False):
+#         self.just_frames = just_frames
+#         self.frames, self.actions = torch.load(fn)
+
         
-        self.transform = transform
+#         self.transform = transform
 
-    def __len__(self):
-        return self.frame_data.shape[0]
+#     def __len__(self):
+#         return len(self.frames)
 
-    def __getitem__(self, idx):
-        frame = self.frame_data[idx]
-        if self.transform:
-            frame = self.transform(frame)
-        if self.just_frames:
-            return frame
-        else:
-            action = self.action_data[idx]
-            return frame,action
+#     def __getitem__(self, idx):
+#         frame = self.frames[idx]
+#         if self.transform:
+#             frame = self.transform(frame)
+#         if self.just_frames:
+#             return frame
+#         else:
+#             action = self.actions[idx]
+#             return frame,action
+
+
+# In[24]:
+
 
 transforms = Compose([ToTensor(),Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])])
-rds = FrameDataset(fn,fna,transform=transforms)
-train_loader = DataLoader(rds,batch_size=BATCH_SIZE,shuffle=True) # hopefully this will encourage vae to learn curves
+rds = ImageFolder(os.path.dirname(imdir),transform=transforms)
 
 
-model = AutoEncoder(INPUT_DIM, DIM, K).to(DEVICE)
-opt = torch.optim.Adam(model.parameters(), lr=LR)
+# In[25]:
+
+
+train_loader = DataLoader(rds,batch_size=args.batch_size,shuffle=True,num_workers=args.num_workers) 
+
+
+# In[26]:
+
+
+model = AutoEncoder(INPUT_DIM, args.dim, args.K).to(DEVICE)
+opt = torch.optim.Adam(model.parameters(), lr=args.lr)
 
 def train():
     train_loss = []
@@ -135,7 +200,7 @@ def train():
         loss_vq.backward(retain_graph=True)
 
         # Commitment objective
-        loss_commit = LAMDA * F.mse_loss(z_e_x, z_q_x.detach())
+        loss_commit = args.lambda_ * F.mse_loss(z_e_x, z_q_x.detach())
         loss_commit.backward()
         opt.step()
 
@@ -181,21 +246,25 @@ def generate_samples():
         nrow=8
     )
 
-BEST_LOSS = 999
-LAST_SAVED = -1
-for epoch in range(1, N_EPOCHS):
-    print("Epoch {}:".format(epoch))
-    train()
-    cur_loss, _ = test()
 
-    if cur_loss <= BEST_LOSS:
-        BEST_LOSS = cur_loss
-        LAST_SAVED = epoch
-        print("Saving model!")
-        torch.save(model.state_dict(), 'models/{}_autoencoder.pt'.format(DATASET))
-    else:
-        print("Not saving model! Last saved: {}".format(LAST_SAVED))
+# In[27]:
 
-    generate_samples()
 
+if __name__ == "__main__":
+    BEST_LOSS = 999
+    LAST_SAVED = -1
+    for epoch in range(1, args.epochs):
+        print("Epoch {}:".format(epoch))
+        train()
+        cur_loss, _ = test()
+
+        if cur_loss <= BEST_LOSS:
+            BEST_LOSS = cur_loss
+            LAST_SAVED = epoch
+            print("Saving model!")
+            torch.save(model.state_dict(), 'models/{}_autoencoder.pt'.format(args.dataset))
+        else:
+            print("Not saving model! Last saved: {}".format(LAST_SAVED))
+
+        generate_samples()
 
