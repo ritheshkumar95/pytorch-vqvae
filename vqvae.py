a39c2458d7a3d1ae9180577635252ef66fc51093
Roller44 <roller44@163.com>
2023-07-11 10:49:33 +0800

    correct error shape of transform for mnist dataset.

--- vqvae.py
+++ vqvae.py
@@ -64,7 +64,6 @@ def main(args):
     if args.dataset in ['mnist', 'fashion-mnist', 'cifar10']:
         transform = transforms.Compose([
             transforms.ToTensor(),
-            # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             transforms.Normalize((0.5), (0.5))
         ])
         if args.dataset == 'mnist':
